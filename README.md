c51c8bc1339a206811be0f22bec97cc9b5c11925
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-28 14:02:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# t6-forum-api
+ 
---
+++ src/test/java/io/github/lucaspicinini/t6_forum_api/T6ForumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucaspicinini.t6_forum_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T6ForumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
