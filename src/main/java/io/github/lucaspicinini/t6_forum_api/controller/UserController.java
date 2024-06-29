2cc80394ea14f6762b09de9a6c1655dd632619c4
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-29 00:15:07 -0300

    Implements List, Update, Delete and Detail for User entity

---
+++ src/main/java/io/github/lucaspicinini/t6_forum_api/controller/UserController.java
@@ -0,0 +1,53 @@
+package io.github.lucaspicinini.t6_forum_api.controller;
+
+import io.github.lucaspicinini.t6_forum_api.dto.UserDetailsDto;
+import io.github.lucaspicinini.t6_forum_api.dto.UserUpdateDto;
+import io.github.lucaspicinini.t6_forum_api.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<Page<UserDetailsDto>> list(Pageable pageable) {
+        var page = userService.list(pageable).map(UserDetailsDto::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody @Valid UserUpdateDto dto) {
+        if (userService.update(dto) != null) {
+            var user = userService.update(dto);
+
+            return ResponseEntity.ok(new UserDetailsDto(user));
+        }
+
+        return ResponseEntity.status(401).body("Acesso não autorizado.");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        if (userService.delete(id)) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.status(401).body("Acesso não autorizado.");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDetailsDto> detail(@PathVariable Long id) {
+        var user = userService.detail(id);
+
+        return ResponseEntity.ok(new UserDetailsDto(user));
+    }
+}
